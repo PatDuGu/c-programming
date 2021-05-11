f5024dddd506a80f308a3de3a202aaec357335c5
Coursera Learner <nobody@nowhere.nul>
2021-05-11 20:17:24 +0000

    Did assigment

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -7,14 +7,17 @@ size_t maxSeq (int * array, size_t n){
     return 0;
       }
   size_t Lenght=1;
-  size_t LenghtMax;
+  size_t LenghtMax=1;
   for(size_t i=1;i<n;i++){
     if (array[i]>array[i-1]){
       Lenght++;
-      LenghtMax=Lenght;
+     
     }
     else {
       Lenght=1;
+    }
+    if (Lenght>LenghtMax){
+      LenghtMax=Lenght;
     }
   }
     return LenghtMax;
